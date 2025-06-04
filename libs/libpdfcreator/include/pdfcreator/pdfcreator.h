9dd104787e634c811d7d81615911fc8e0cde3337
ConnCaster <andron5712@yandex.ru>
2025-06-04 16:58:08 +0300

    Реализовано создание заголовка в PDF

--- libs/libpdfcreator/include/pdfcreator/pdfcreator.h
+++ libs/libpdfcreator/include/pdfcreator/pdfcreator.h
@@ -4,6 +4,53 @@
 #include <hpdf.h>
 #include <json.hpp>
 
-void print();
+using json = nlohmann::json;
+
+constexpr std::string_view kFont = "Times-Roman";   // шрифт по умолчанию
+constexpr HPDF_REAL kFontSize = 14.0;               // размер шрифта
+constexpr HPDF_REAL kLineSpacing = 15.0;            // межстрочный интерва
+
+constexpr HPDF_REAL kStartPosX = 20;
+constexpr HPDF_REAL kStartPosY = 20;
+constexpr HPDF_REAL kMargin = 20;                   // размер воображаемой рамки страницы (одинаковый со всех сторон)
+
+class IDocument {
+public:
+    static const std::vector<std::string> kHeaders_;
+
+public:
+    virtual ~IDocument() = default;
+
+    virtual void AddHeader(const json& header_fields) {};
+    virtual void AddBody() {};
+    virtual void AddFooter() {};
+
+    virtual void SaveToFile(const std::string& file_path) = 0;
+};
+
+class PDFDocument : public IDocument {
+public:
+    PDFDocument();
+
+    void AddHeader(const json& header_fields) override;
+    void SaveToFile(const std::string& file_path) override;
+
+    ~PDFDocument() override;
+
+private:
+    void AddNewPage();
+    void SetupFont();
+
+private:
+    HPDF_Doc pdf_;
+    HPDF_Page page_;
+    HPDF_Font font_;
+
+    struct Cursor {
+        HPDF_REAL x = kStartPosX;
+        HPDF_REAL y = kStartPosY;
+    } cursor_;
+
+};
 
 #endif
--- libs/libpdfcreator/src/pdfcreator.cpp
+++ libs/libpdfcreator/src/pdfcreator.cpp
@@ -2,6 +2,83 @@
 
 #include <iostream>
 
-void print() {
-    std::cout << "PDF creator" << std::endl;
+const std::vector<std::string> IDocument::kHeaders_ = {
+    "integrityid",
+    "type_id",
+    "journal_id",
+    "time",
+    "result",
+    "info",
+    "object",
+    "printer",
+    "user_name"
+};
+
+PDFDocument::PDFDocument() {
+    pdf_ = HPDF_New(nullptr, nullptr);
+    if (!pdf_) {
+        throw std::runtime_error("Error creating pdf document");
+    }
+
+    // Настройка параметров страницы
+    page_ = HPDF_AddPage(pdf_);
+    if (!page_) {
+        throw std::runtime_error("Error creating new page in pdf");
+    }
+    HPDF_Page_SetSize(page_, HPDF_PAGE_SIZE_A4, HPDF_PAGE_PORTRAIT);
+    cursor_.y = HPDF_Page_GetHeight(page_) - kStartPosY;
+
+    // Настройка шрифта
+    SetupFont();
+}
+
+PDFDocument::~PDFDocument() {
+    HPDF_Free(pdf_);
+}
+
+void PDFDocument::AddHeader(const json& header_fields) {
+    if (header_fields.empty()) return;
+
+    for (const auto& field : header_fields) {
+        if (cursor_.y < kMargin) {
+            std::cout << "Создание новой страницы" << std::endl;
+            try {
+                AddNewPage();
+            } catch (std::exception& e) {
+                throw;
+            }
+        }
+
+        std::string text = field.at("name").get<std::string>() + ": " + field.at("value").get<std::string>();
+
+        HPDF_Page_BeginText(page_);
+        HPDF_Page_TextOut(page_, kStartPosX, cursor_.y, text.c_str());
+        HPDF_Page_EndText(page_);
+        cursor_.y -= kLineSpacing;
+        std::cout << "Current Y: " << cursor_.y << ", Page height: " << HPDF_Page_GetHeight(page_) << std::endl;
+    }
+}
+
+void PDFDocument::SaveToFile(const std::string& file_path) {
+    HPDF_SaveToFile(pdf_, file_path.data());
+}
+
+void PDFDocument::AddNewPage() {
+    page_ = HPDF_AddPage(pdf_);
+    if (!page_) {
+        throw std::runtime_error("Error creating new page in pdf");
+    }
+    HPDF_Page_SetSize(page_, HPDF_PAGE_SIZE_A4, HPDF_PAGE_PORTRAIT);
+    cursor_.y = HPDF_Page_GetHeight(page_) - kMargin;
+    HPDF_Page_SetFontAndSize(page_, font_, kFontSize);
+}
+
+void PDFDocument::SetupFont() {
+    const char* font_name = HPDF_LoadTTFontFromFile(pdf_, "/home/user/dir/PDFCreator/fonts/JetBrainsMonoNL-Regular.ttf", HPDF_TRUE);
+    HPDF_UseUTFEncodings(pdf_);
+    font_ = HPDF_GetFont(pdf_, font_name, "UTF-8");
+    if (!font_) {
+        font_ = HPDF_GetFont(pdf_, kFont.data(), nullptr);
+    }
+    HPDF_Page_SetFontAndSize(page_, font_, kFontSize);
 }
